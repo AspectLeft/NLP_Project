596f1b77cbde934f8e5e2bd279f2dfa2e9e861ad
AspectLeft <aspectleft@gmail.com>
2019-12-04 16:39:41 -0800

    Update aen.py
    Adding some comments

--- models/aen.py
+++ models/aen.py
@@ -10,6 +10,7 @@ import torch.nn.functional as F
 import numpy as np
 
 
+# Baseline
 class AEN_GloVe(nn.Module):
     def __init__(self, embedding_matrix, opt):
         super(AEN, self).__init__()
@@ -53,7 +54,7 @@ class AEN_GloVe(nn.Module):
         out = self.dense(x)
         return out
 
-
+# Baseline
 class AEN_BERT(nn.Module):
     def __init__(self, bert, opt):
         super(AEN_BERT, self).__init__()
@@ -100,7 +101,8 @@ class AEN_BERT(nn.Module):
         out = self.dense(x)
         return out
 
-
+# Template during development
+# We tried many other methods, such as CDM
 class AEN_BERT_SIMPLE(nn.Module):
     def __init__(self, bert, opt):
         super(AEN_BERT_SIMPLE, self).__init__()
@@ -200,7 +202,7 @@ class AEN_BERT_SIMPLE(nn.Module):
         masked_text_raw_indices = torch.from_numpy(masked_text_raw_indices)
         return masked_text_raw_indices.to(self.opt.device)
 
-
+# Our final model
 class AEN_SIMPLE(nn.Module):
     def __init__(self, bert, opt):
         super(AEN_SIMPLE, self).__init__()
@@ -209,14 +211,11 @@ class AEN_SIMPLE(nn.Module):
         self.squeeze_embedding = SqueezeEmbedding()
         self.dropout = nn.Dropout(opt.dropout)
 
-        # self.attn_k = Attention(opt.bert_dim, out_dim=opt.hidden_dim, n_head=8, score_function='mlp', dropout=opt.dropout)
         self.attn_q = Attention(opt.bert_dim, out_dim=opt.hidden_dim, n_head=opt.mha_heads, score_function='mlp',
                                 dropout=opt.dropout)
-        # self.ffn_c = PositionwiseFeedForward(opt.hidden_dim, dropout=opt.dropout)
         self.ffn_t = PositionwiseFeedForward(opt.hidden_dim, dropout=opt.dropout)
 
         self.lstm = nn.LSTM(opt.hidden_dim, opt.lstm_hid, opt.lstm_layer, batch_first=True, bidirectional=opt.lstm_bidir)
-        # self.attn_s1 = Attention(opt.hidden_dim, n_head=8, score_function='mlp', dropout=opt.dropout)
 
         self.dense = nn.Linear(opt.lstm_hid * (2 if opt.lstm_bidir else 1), opt.polarities_dim)
 
@@ -224,55 +223,24 @@ class AEN_SIMPLE(nn.Module):
         context, target = inputs[0], inputs[1]
         context_len = torch.sum(context != 0, dim=-1)
         target_len = torch.sum(target != 0, dim=-1)
+        # BERT/RoBERTa embedding
         context = self.squeeze_embedding(context, context_len)
         context, _ = self.bert(context)
         context = self.dropout(context)
-        # CDM
-        # context = torch.mul(context,
-        #                     self.squeeze_embedding(self.feature_dynamic_mask(inputs[0], inputs[1]), context_len))
         target = self.squeeze_embedding(target, target_len)
         target, _ = self.bert(target)
         target = self.dropout(target)
 
-        # hc, _ = self.attn_k(context, context)
-        # hc = self.ffn_c(hc)
+        # Attention encoding
         ht, _ = self.attn_q(context, target)
+        # PCT
         ht = self.ffn_t(ht)
 
-        # s1, _ = self.attn_s1(hc, ht)
-
-        # context_len = torch.tensor(context_len, dtype=torch.float).to(self.opt.device)
         target_len = torch.tensor(target_len, dtype=torch.float).to(self.opt.device)
 
-        # hc_mean = torch.div(torch.sum(hc, dim=1), context_len.view(context_len.size(0), 1))
-        # ht_mean = torch.div(torch.sum(ht, dim=1), target_len.view(target_len.size(0), 1))
+        # LSTM decoding
         ht_mean, _ = self.lstm(ht)
         ht_mean = ht_mean[:, -1, :]
-        # s1_mean = torch.div(torch.sum(s1, dim=1), context_len.view(context_len.size(0), 1))
 
-        # x = torch.cat((hc_mean, s1_mean, ht_mean), dim=-1)
         out = self.dense(ht_mean)
         return out
-
-    def feature_dynamic_mask(self, text_local_indices, aspect_indices):
-        texts = text_local_indices.cpu().numpy()
-        asps = aspect_indices.cpu().numpy()
-        mask_len = self.opt.SRD
-        masked_text_raw_indices = np.ones((text_local_indices.size(0), self.opt.max_seq_len, self.opt.bert_dim),
-                                          dtype=np.float32)
-        for text_i, asp_i in zip(range(len(texts)), range(len(asps))):
-            asp_len = np.count_nonzero(asps[asp_i]) - 2
-            try:
-                asp_begin = np.argwhere(texts[text_i] == asps[asp_i][1])[0][0]
-            except:
-                continue
-            if asp_begin >= mask_len:
-                mask_begin = asp_begin - mask_len
-            else:
-                mask_begin = 0
-            for i in range(mask_begin):
-                masked_text_raw_indices[text_i][i] = np.zeros((self.opt.bert_dim), dtype=np.float)
-            for j in range(asp_begin + asp_len + mask_len, self.opt.max_seq_len):
-                masked_text_raw_indices[text_i][j] = np.zeros((self.opt.bert_dim), dtype=np.float)
-        masked_text_raw_indices = torch.from_numpy(masked_text_raw_indices)
-        return masked_text_raw_indices.to(self.opt.device)
